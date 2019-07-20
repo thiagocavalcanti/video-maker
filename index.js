6155ba866cb121090e90d3c491b39109b0f42d9e
Thiago Oliveira <thiago.da.c.oliveira@accenture.com>
2019-07-20 13:50:33 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-const readLine = require('readline-sync');
+const robots = {
+  text: require('./robots/text'),
+  userInput: require('./robots/user-input')
+};
 
-function start() {
+async function start() {
   const content = {};
-  content.searchTerm = askAndReturnSearchTerm();
-  content.prefix = askAndReturnPrefix();
 
-  function askAndReturnSearchTerm() {
-    return readLine.question('Escreva um termo de pesquisa para Wikipedia: ');
-  }
-  function askAndReturnPrefix() {
-    const prefixes = [
-      'Quem e',
-      'O que e',
-      'A historia de',
-      'A historia da',
-      'A historia do'
-    ];
-    const prefixesReal = [
-      'Quem é',
-      'O que é',
-      'A história de',
-      'A história da',
-      'A história do'
-    ];
-    const selectedPrefixIndex = readLine.keyInSelect(
-      prefixes,
-      'Selecione uma opcao',
-      { encoding: 'utf8' }
-    );
-    return prefixesReal[selectedPrefixIndex];
-  }
+  robots.userInput(content);
+  await robots.text(content);
+
   console.log(content);
 }
 
